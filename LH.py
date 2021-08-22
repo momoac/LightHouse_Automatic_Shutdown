fcc3a6cd0f59525029e5abba5175f8fe5e2ad1f6
momoac <89334739+momoac@users.noreply.github.com>
2021-08-22 18:04:54 +0800

    Update LH.py

--- LH.py
+++ LH.py
@@ -2,6 +2,7 @@ import json
 import time
 import requests
 import os
+import requests
 from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
