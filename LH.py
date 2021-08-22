0bebb3941aab27944a1d19d8110b65086e0b36ea
momoac <89334739+momoac@users.noreply.github.com>
2021-08-22 18:06:56 +0800

    Update LH.py

--- LH.py
+++ LH.py
@@ -115,9 +115,12 @@ def dofetch(id, key, region):
                 print(resp_Stop.to_json_string())
                 #添加TG酱通知
                 msgContent= InstanceId+ " ：流量超出限制，即将自动关机。" + "剩余流量：" + TrafficPackageRemaining+ "GB"
-                msgUrl="https://api.telegram.org/bot1737507586:AAG-JXpVzPLDG3vQWAaCX0_SJfZPMPS1WbU/sendMessage?chat_id=1095779157&text="+ msgContent
+                msgUrl="https://api.telegram.org/bot1737507586:AAG-JXpVzPLDG3vQWAaCX0_SJfZPMPS1WbU/sendMessage?chat_id=1095779157&text=测试"+ msgContent
                 #告警结果：
                 gaojinResult="流量告警结果：流量超出限制，即将自动关机。\n"+"剩余流量：" + str(TrafficPackageRemaining)+ "GB"
+                r = requests.get(msgUrl)
+                print r.status_code
+                print r.content
                 response= requests.get(url=msgUrl)
                 print (response)        
         else:
